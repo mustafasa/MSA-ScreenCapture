8b029809d5c54e6cb8699845fe06969aae2be18a
ENT\arifm2 <mustafa.arif@medtronic.com>
2017-10-18 15:07:00 -0700

    Added Example

---
+++ SampleApp/src/androidTest/java/com/msa/mustafasyedarif/androidcapturesample/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.msa.mustafasyedarif.androidcapturesample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.msa.mustafasyedarif.screenshotlibrary.MSA;
+import com.msa.mustafasyedarif.screenshotlibrary.ScreenIdentifier;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by arifm2 on 10/18/2017.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> intentsTestRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
+    @Test
+    public void test() throws InterruptedException {
+        //Recommend to add sleep thread,Expresso thread is asyn to UI thread result improper capturation
+        Thread.sleep(750);
+
+        //This method will create screenshot of current view, returns boolean.
+        boolean success= MSA.CaptureScreenShot(intentsTestRule.getActivity());
+
+        //This method will create screenshot of current view with desire name.
+        //Note: If used repeated name,results in overriding existing images.
+        boolean success2=MSA.CaptureScreenShot(intentsTestRule.getActivity(),"desirename");
+
+        //This method capture meta-data of UI elemnts and return back with arraylist of screenIdentifier.
+        ArrayList<ScreenIdentifier> identifier= MSA.CaptureScreenIdentifier(intentsTestRule.getActivity());
+    }
+
+}
--- SampleApp/src/main/java/com/msa/mustafasyedarif/androidcapturesample/MainActivity.java
+++ SampleApp/src/main/java/com/msa/mustafasyedarif/androidcapturesample/MainActivity.java
@@ -1,16 +1,62 @@
 package com.msa.mustafasyedarif.androidcapturesample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.msa.mustafasyedarif.screenshotlibrary.MSA;
+import com.msa.mustafasyedarif.screenshotlibrary.ScreenIdentifier;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "MSA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MSA.CaptureScreenShot(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    public void takeScreenshot(View view) {
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            return;
+        }
+        //This method will create screenshot of current view, returns boolean.
+        boolean success = MSA.CaptureScreenShot(this);
+        Log.d(TAG, String.valueOf(success));
+        //This method will create screenshot of current view with desire name.
+        //Note: If used repeated name,results in overriding existing images.
+        boolean successAgain = MSA.CaptureScreenShot(this, "desirename");
+        Log.d(TAG, String.valueOf(successAgain));
+
+        //This method capture meta-data of UI elemnts and return back with arraylist of
+        // screenIdentifier.
+        ArrayList<ScreenIdentifier> identifier = MSA.CaptureScreenIdentifier(this);
+
+        //if you loop through arraylist, it provide each element information
+        //eg:
+        ScreenIdentifier firstUIElement = identifier.get(0);
+        Log.d(TAG, firstUIElement.getUiIdentifier());
+        Log.d(TAG, firstUIElement.getUiType());
+        ///and many more attributes are available.....
+
+
     }
 }
